13e7d01e788b1a98e99a8da3e7ca4ae031f1c4dc
crowdbotics-dev <engineering@crowdbotics.com>
2022-12-15 17:17:41 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,8 @@
+from django.conf import settings
 from django.db import models
+class Test(models.Model):
+    'Generated Model'
+    title = models.CharField(max_length=256,)
+    def test_method(self):
+        response = requests.get('https://hello.com')
+        return response
